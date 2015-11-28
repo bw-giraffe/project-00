1f5547fb652ad9795a1387809fdeb159664c6968
bw-giraffe <theletterrsquared@gmail.com>
2015-11-27 18:12:21 -0800

    got a really basic game working with two basic squares racing across the canvas, controlled by a different key for each one

--- game.js
+++ game.js
@@ -1,34 +1,79 @@
 $(document).ready(function(){
-	//Canvas stuff
-	var canvas = $("#canvas")[0];
-	var ctx = canvas.getContext("2d");
-	var w = $("#canvas").width();
-	var h = $("#canvas").height();
-	
-	var xpos = 10; //default
-	var ypos = 10;
-
-	function paint() {
-
-	ctx.fillStyle = "white";
-	ctx.fillRect(0, 0, w, h);
-	ctx.strokeStyle = "black";
-	ctx.strokeRect(0, 0, w, h);
-
-	ctx.rect(xpos, 10, 150, 100);
-	ctx.stroke();
-	}
-
-
-
-	$(document).keydown(function(e) {
-		var key = e.which;
-	 	if(key == "39") {
-	 		xpos = xpos + 1;
-	 		console.log("you pressed this key")
-	 	}
-    })
-	
-	paint();
-	
+	console.log("the document is ready!");
 })
+
+var canvas_width = 480;
+var canvas_height = 320;
+
+var canvasElement = $("<canvas width='" + canvas_width + 
+                      "' height='" + canvas_height + "'></canvas>");
+var canvas = canvasElement.get(0).getContext("2d");
+canvasElement.appendTo('body');
+
+var recOneX = 10;
+var recOneY = 10;
+
+var recTwoX = 10;
+var rectTwoY = 50; 
+
+/* function rectangleOne() {
+  canvas.fillStyle = "#ABEBC6";
+  canvas.fillRect(recOneX, recOneY, 20, 20);
+} */
+
+
+//where we get the keypress 
+document.onkeydown = function (event) {
+	if(event.keyCode == '83') {
+			console.log("bump rec1");
+			recOneX = recOneX + 5;
+	} else if(event.keyCode == '75') {
+			console.log("bump rec2");
+			recTwoX = recTwoX + 5;
+	}
+}
+
+
+function rectOne() {
+	canvas.beginPath();
+	canvas.fillStyle = "#A00";
+	canvas.rect(recOneX, 20, 50, 50);
+	canvas.fill();
+}
+
+function rectTwo() {
+	canvas.beginPath();
+	canvas.fillStyle = "#3AC301";
+	canvas.rect(recTwoX, 200, 50, 50);
+	canvas.fill();
+}
+
+	
+
+var FPS = 30;
+setInterval(function() {
+	update();
+	draw();
+}, 1000/FPS);
+
+var textX = 50;
+var textY = 50;
+
+function update() {
+	textX += 1;
+	textY += 1;
+ }
+
+function draw() { 
+
+	canvas.clearRect(0, 0, canvas_width, canvas_height);
+	rectOne();
+	rectTwo();
+	canvas.fillStyle = "#00A";
+	canvas.fillText("Race!", textX, textY);
+
+}
+
+
+
+
